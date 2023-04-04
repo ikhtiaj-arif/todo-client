30ae86c61ce5953f6e16f3511946f303a3758b64
AnGon777 <ikhtiaj.arif@gmail.com>
2023-04-04 16:34:44 +0600

    logo added

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
+import icon from "../assects/logo_no_bg.png";
 import { AuthContext } from "../context/userContext";
 
 const Nav = () => {
@@ -13,43 +14,38 @@ const Nav = () => {
       .catch((e) => toast.error(e));
   };
   return (
-    <div className="navbar md:w-3/4 mx-auto bg-base-100">
-      <div className="flex-1 text-2xl font-semibold to-white">
-        <Link to="/">Todo App</Link>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          {user && user?.uid ? (
-            <li>
-              <Link to="/todos">All Todos</Link>
-            </li>
-          ) : (
-            <li></li>
-          )}
-
-          <li>
-            {user ? (
-              <button
-                className="btn bg-violet-500 text-white"
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
+    <div className="navbar  bg-gray-700">
+      <div className="md:w-3/4 mx-auto">
+        <Link className="flex-1 flex items-center text-2xl font-semibold to-white">
+          <img src={icon} alt="" className="h-[105px]" />
+          Todo App
+        </Link>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            {user && user?.uid ? (
+              <li>
+                <Link to="/todos">All Todos</Link>
+              </li>
             ) : (
-              <Link to="/login">Login</Link>
+              <li></li>
             )}
-          </li>
-        </ul>
+
+            <li>
+              {user ? (
+                <button
+                  className="btn bg-violet-500 text-white"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-    // <div>
-    //   <Link to="/register">Register</Link>||
-    //   {user ? (
-    //     <button onClick={handleLogout}>Logout</button>
-    //   ) : (
-    //     <Link to="/login">Login</Link>
-    //   )}
-    // </div>
   );
 };
 
